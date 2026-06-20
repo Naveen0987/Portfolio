ed5244b59ace3236d3442e4cf7acc6f2013b349c
Naveen0987 <naveenchcti034.calctechnologies@gmail.com>
2026-06-20 19:06:41 +0530

    added whatsapp button

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,14 @@ function App() {
       <Background />
       <ScrollToTop />
       <Home />
+     <a
+  href="https://wa.me/918639868030?text=Hi%20Naveen,%20I%20visited%20your%20portfolio."
+  className="whatsapp-btn"
+  target="_blank"
+  rel="noreferrer"
+>
+  <FaWhatsapp />
+</a>
     </>
   );
 }
